922fa8032cb5169127017378011462eb155c1508
Tiago <o-seu-email-do-github@exemplo.com>
2025-11-06 12:10:33 +0000

    Aula 25: Estilizei o menu mobile (agora está profissional!)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -15,22 +15,33 @@ function Header() {
         DocFacil.pt
       </a>
 
+      <nav className={styles.menuDesktop}>
+
+          <a href="#" className={styles.linkDesktop}>Funcionalidades</a>
+          <a href="#" className={`${styles.linkDesktop} ${styles.ativo}`}>Como Funciona</a>
+          <a href="#" className={styles.linkDesktop}>Testemunhos</a>
+          <a href="#" className={styles.linkDesktop}>Preços</a>
+          <a href="#" className={styles.linkDesktop}>FAQ</a>
+          <a href="#" className={styles.linkDesktop}>Login</a>
+        </nav>
+
       <div 
         className={styles.menuIcon} 
         onClick={() => setMenuAberto(true)}
       >
         {iconeMenu}
-
       </div>
       
     </header>
 
     {menuAberto && (
      <div className={styles.menuMobileAberto}>
-        <div className={styles.menuIcon} onClick={() => setMenuAberto(false)}>
-            X
-            </div>
-            <a href="#" className={styles.linkMenuMobile}>Funcionalidades</a>
+        <div className={styles.menuIcon} onClick={() => setMenuAberto(false)}
+         >
+         x
+    </div>
+    
+    <a href="#" className={styles.linkMenuMobile}>Funcionalidades</a>
     <a href="#" className={styles.linkMenuMobile}>Como Funciona</a>
     <a href="#" className={styles.linkMenuMobile}>Testemunhos</a>
     <a href="#" className={styles.linkMenuMobile}>Preços</a>
