6418be52b4fa93257655a6d361a0adf3f3f79383
Tiago <o-seu-email-do-github@exemplo.com>
2025-11-06 07:44:14 +0000

    Aula 24: Adicionei 'State' (useState) e 'onClick' ao Header. O menu agora abre e fecha.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,39 @@
+import { useState } from 'react';
 import styles from './Header.module.css';
 
-function Header () {
-    
-    const iconeMenu = '☰';
-    
-    return (
-        <header className={styles.navBar}>
-            
-            <a href="#" className={styles.logo}>DocFacil.pt</a>
+function Header() {
+  
+  const [menuAberto, setMenuAberto] = useState(false);
+  
+  const iconeMenu = '☰';
 
-            <div className={styles.menuIcon}>{iconeMenu}</div>
-            </header>
-    );
+  return (
+    <>
+    <header className={styles.navBar}>
+      
+      <a href="#" className={styles.logo}>
+        DocFacil.pt
+      </a>
+
+      <div 
+        className={styles.menuIcon} 
+        onClick={() => setMenuAberto(true)}
+      >
+        {iconeMenu}
+        
+      </div>
+      
+    </header>
+
+    {menuAberto && (
+      <div>
+      <h2>O MENU ESTÁ ABERTO!</h2>
+      <p>Aqui vão estar os nossos links</p>
+      <button onClick={() => setMenuAberto(false)}>Fechar Menu</button>
+      </div>
+    )}
+    </>
+  );
 }
 
 export default Header;
