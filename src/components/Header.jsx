b6c920757f34e5ea135db7f6b497a6a1987325d7
Tiago <o-seu-email-do-github@exemplo.com>
2025-11-06 08:54:50 +0000

    Aula 25: Estilizei o menu mobile (agora está profissional!)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -20,17 +20,24 @@ function Header() {
         onClick={() => setMenuAberto(true)}
       >
         {iconeMenu}
-        
+
       </div>
       
     </header>
 
     {menuAberto && (
-      <div>
-      <h2>O MENU ESTÁ ABERTO!</h2>
-      <p>Aqui vão estar os nossos links</p>
-      <button onClick={() => setMenuAberto(false)}>Fechar Menu</button>
-      </div>
+     <div className={styles.menuMobileAberto}>
+        <div className={styles.menuIcon} onClick={() => setMenuAberto(false)}>
+            X
+            </div>
+            <a href="#" className={styles.linkMenuMobile}>Funcionalidades</a>
+    <a href="#" className={styles.linkMenuMobile}>Como Funciona</a>
+    <a href="#" className={styles.linkMenuMobile}>Testemunhos</a>
+    <a href="#" className={styles.linkMenuMobile}>Preços</a>
+    <a href="#" className={styles.linkMenuMobile}>FAQ</a>
+    <a href="#" className={styles.linkMenuMobile}>Login</a>
+
+     </div>
     )}
     </>
   );
